46fdb8ed3f152aff644aa34159c20015ef109d3e
iskako01 <85966697+iskako01@users.noreply.github.com>
2022-04-14 20:21:05 +0200

    Start

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,41 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
   extends: [
-    'plugin:vue/vue3-essential',
-    '@vue/standard',
-    '@vue/typescript/recommended'
+    "plugin:vue/vue3-essential",
+    "@vue/standard",
+    "@vue/typescript/recommended",
   ],
   parserOptions: {
-    ecmaVersion: 2020
+    ecmaVersion: 2020,
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
-}
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    camelcase: "off",
+    quotes: [2, "double", { avoidEscape: true }],
+    indent: ["error", 2, { SwitchCase: 1 }],
+    semi: [2, "always"],
+    "standard/no-callback-literal": 0,
+    "comma-dangle": [
+      "error",
+      {
+        arrays: "always-multiline",
+        exports: "always-multiline",
+        functions: "never",
+        imports: "always-multiline",
+        objects: "always-multiline",
+      },
+    ],
+    "space-before-function-paren": [
+      "error",
+      {
+        anonymous: "always",
+        named: "never",
+        asyncArrow: "always",
+      },
+    ],
+  },
+};
