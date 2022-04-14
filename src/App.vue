25b4930ed1ef96970588d045809e44fe667e3b46
iskako01 <85966697+iskako01@users.noreply.github.com>
2022-04-14 21:45:51 +0200

    AppForm component

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,13 @@
 <template>
-  <div class=""></div>
+  <AppForm />
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import AppForm from "@/components/AppForm.vue";
 
 export default defineComponent({
   name: "App",
-  components: {},
+  components: { AppForm },
 });
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/AppForm.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="container">
+    <form @submit.prevent class="card">
+      <div class="form-control">
+        <label for="primaryColor">primaryColor</label>
+        <input type="text" id="primaryColor" v-model="options.primaryColor" />
+      </div>
+
+      <div class="form-control">
+        <label for="borderRadius">borderRadius</label>
+        <input type="number" id="borderRadius" v-model="options.borderRadius" />
+      </div>
+
+      <div class="form-control">
+        <label for="dismissType">dismissType</label>
+        <select id="dismissType" v-model="options.dismissType">
+          <option value="cross">cross</option>
+          <option value="cross-faint">cross-faint</option>
+          <option value="text">text</option>
+        </select>
+      </div>
+
+      <div class="form-control">
+        <label for="expiration">expiration</label>
+        <input type="number" id="expiration" v-model="options.expiration" />
+      </div>
+
+      <div class="form-checkbox">
+        <label for="dismissable">dismissable</label>
+        <input type="checkbox" id="dismissable" v-model="options.dismissable" />
+      </div>
+    </form>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+import iOptions from "@/types/options";
+
+export default defineComponent({
+  name: "AppForm",
+  setup() {
+    const options = ref<iOptions>({
+      dataLayer: "dataLayer",
+      primaryColor: "#f98305",
+      borderRadius: 6,
+      dismissable: false,
+      dismissType: "text",
+      expiration: 365,
+      closeType: "cross",
+    });
+
+    return { options };
+  },
+});
+</script>
